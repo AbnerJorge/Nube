8d21d1e0d6e173a8e93008586ff90dd51191e878
Abner Jorge Gonzales Corzo <202014049@uns.edu.pe>
2025-06-26 00:01:52 -0500

    Update practica.sql cambios a mysql

--- practica.sql
+++ practica.sql
@@ -1,39 +1,34 @@
-CREATE SCHEMA IF NOT EXISTS practica;
+CREATE DATABASE IF NOT EXISTS practica;
 USE practica;
 
--- Eliminamos tablas si existen (Spanner aún no permite DROP IF EXISTS, se simula lógicamente en SQL estándar)
 DROP TABLE IF EXISTS Cliente;
 DROP TABLE IF EXISTS Distrito;
 
--- Crear tabla Distrito
 CREATE TABLE Distrito (
-  codigo            INT64 NOT NULL,
-  nombre            STRING(1024) NOT NULL,
-  CONSTRAINT distrito_pk PRIMARY KEY (codigo)
+  codigo BIGINT NOT NULL,
+  nombre VARCHAR(1024) NOT NULL,
+  PRIMARY KEY (codigo)
 );
 
--- Crear tabla Cliente
 CREATE TABLE Cliente (
-  dni               STRING(8) NOT NULL,
-  razon_social      STRING(1024) NOT NULL,
-  direccion         STRING(1024),
-  telefono          STRING(9),
-  monto             FLOAT64,
-  cod_distrito      INT64,
-  CONSTRAINT cliente_pk PRIMARY KEY (dni),
-  CONSTRAINT cliente_monto_ck CHECK (monto >= 0)
+  dni CHAR(8) NOT NULL,
+  razon_social VARCHAR(1024) NOT NULL,
+  direccion VARCHAR(1024),
+  telefono VARCHAR(9),
+  monto DECIMAL(10,2),
+  cod_distrito BIGINT,
+  PRIMARY KEY (dni),
+  CHECK (monto >= 0)
 );
 
--- Insertar datos en la tabla Distrito
 INSERT INTO Distrito (codigo, nombre) VALUES
-  (1001, "Chimbote"),
-  (1002, "Coishco"),
-  (1003, "Huarmey");
+  (1001, 'Chimbote'),
+  (1002, 'Coishco'),
+  (1003, 'Huarmey');
 
--- Insertar datos en la tabla Cliente
 INSERT INTO Cliente (dni, razon_social, direccion, telefono, monto, cod_distrito) VALUES
-  ("47402210", "Pepito SA", "Av Proceres", "999888777", 30.5, 1003),
-  ("47402211", "Mitzuki SA", "Av Regional", "999666555", 65.5, 1001),
-  ("47402212", "Intel EIRL", "Jr Union", "999444333", 93.0, 1002);
+  ('47402210', 'Pepito SA', 'Av Proceres', '999888777', 30.50, 1003),
+  ('47402211', 'Mitzuki SA', 'Av Regional', '999666555', 65.50, 1001),
+  ('47402212', 'Intel EIRL', 'Jr Union', '999444333', 93.00, 1002);
 
 COMMIT;
